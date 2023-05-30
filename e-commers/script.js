c01485624b0934861be3ef6f06be74d21eeac5d8
Liver Nissan <storegame352@gmail.com>
2023-05-30 20:49:56 +0200

    fetcha till databas

--- e-commers/script.js
+++ e-commers/script.js
@@ -50,9 +50,6 @@ function buyButtonClicked(){
 }
 
 
-
-
-
 function removeCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.remove();
@@ -70,6 +67,7 @@ function quantityChanged(event){
 
 function addCartClick(event){
     var button = event.target
+    console.log(event)
     var shopProducts = button.parentElement
     var title = shopProducts.getElementsByClassName('product-title')[0].innerText;
     var price = shopProducts.getElementsByClassName('price')[0].innerText;
@@ -78,34 +76,7 @@ function addCartClick(event){
     updatetotal();
 }
 
-// function addProductToCart(title, price, productImg) {
-//     var cartShopBox = document.createElement('div');
-//     cartShopBox.classList.add('cart-box');
-//     var cartItems = document.getElementsByClassName('cart-content')[0];
-//     var cartItemsNames = cartItems.getElementsByClassName('cart-product-title')
-//     for (var i = 0; i < cartItemsNames.length; i++){
-//         alert("Du har redan lagt till kort");
-//         return;
-//     }
-    
-// }
 
-// var cartBoxContent = `
-//                         <img src="${productImg}" alt="" class="cart-img">
-//                         <div class="detail-box">
-//                             <div class="cart-product-title">${title}</div>
-//                             <div class="cart-price">${price}</div>
-//                             <input type="number" value="1" class="cart-quantity">
-//                         </div>
-//                         <i class='bx bxs-trash cart-remove'></i>`;
-// cartShopBox.innerHTML = cartBoxContent
-// cartItems.append(cartShopBox)
-// cartShopBox
-//             .getElementsByClassName('cart-remove')[0]
-//             .addEventListener('click', removeCartItem);
-// cartShopBox
-//             .getElementsByClassName('cart-quantity')[0]
-//             .addEventListener('change', quantityChanged);
 function addProductToCart(title, price, productImg) {
     var cartShopBox = document.createElement("div");
     cartShopBox.classList.add("cart-box");
@@ -138,24 +109,7 @@ function addProductToCart(title, price, productImg) {
 
 
 
-// function updatetotal(){
-//     var cartContent = document.getElementsByClassName('cart-content')[0];
-//     var cartBoxes = document.getElementsByClassName('cart-box')[0];
-//     var total = 0;
-//     for (var i = 0; i < cartBoxes.length; i++){
-//         var cartBox = cartBoxes[i];
-//         var priceElement = cartBox.getElementsByClassName('cart-price')[0];
-//         var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
-//         var price = parseFloat(priceElement.innerText.replace("$", ""));
-//         var quantity = quantityElement.value;
-//         total = total + (price * quantity);
-//     }
-//         total = Math.round(total * 100) / 100;
 
-
-//         document.getElementsByClassName('total-price')[0].innerText = "$" + total;
-    
-// }
 
 function updatetotal() {
     var cartBoxes = document.getElementsByClassName("cart-box");
@@ -171,3 +125,60 @@ function updatetotal() {
     total = Math.round(total * 100) / 100;
     document.getElementsByClassName("total-price")[0].innerText = "$" + total;
   }
+
+  fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => {
+    
+    console.log(data);
+    displayProducts(data);
+  })
+  .catch(error => {
+    console.error('Det uppstod ett fel:', error);
+  });
+
+  function displayProducts(data) {
+    var productsContainer = document.querySelector('.products-container');
+  
+    for (var i = 0; i < data.length; i++) {
+      var product = data[i];
+     
+      var productElement = document.createElement("div");
+      productElement.classList.add("product-item");
+  
+     
+
+      var productBox = document.createElement("div")
+      productBox.classList.add("product-box");
+
+      var productImg = document.createElement("img");
+      productImg.classList.add("product-img");
+      productImg.src = product.image
+      var productTitle = document.createElement("h2")
+      productTitle.classList.add("product-title");
+      productTitle.innerText = product.title;
+
+      var price = document.createElement("span")
+      price.classList.add("price");
+      price.innerText = product.price
+
+      var addProduct = document.createElement("i")
+      addProduct.classList.add("bx", "bxs-shopping-bag", "add-cart");
+
+     
+      addProduct.addEventListener("click", (e) => addCartClick(e))
+      
+      
+
+      productBox.appendChild(productImg)
+      productBox.appendChild(productTitle)
+      productBox.appendChild(price)
+      productBox.appendChild(addProduct)
+  
+      productElement.appendChild(productBox);
+  
+      productsContainer.appendChild(productElement);
+ 
+    }
+  }
+
